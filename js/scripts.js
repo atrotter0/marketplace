48d8ebefc5a60c77158f4965fe6e7f93be5d0fbd
Abel Trotter and Rita Bennett-Chew <abel@>
2018-05-31 11:40:38 -0700

    Add business logic, add order confirmation content, add more styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+function firstLetterCaps(item) {
+  var remainder = item.split(item.charAt(0))[1];
+  var firstLetter = item.charAt(0).toUpperCase();
+  var item = firstLetter + remainder;
+  return item;
+}
+
+function addOrderDetails(firstName, lastName, address, item) {
+  $("#first-name-confirm").text(firstName);
+  $("#last-name-confirm").text(lastName);
+  $("#address-confirm").text(address);
+  $("#dino-confirm").text(item);
+}
+
 $(document).ready(function(){
   $("#submit").click(function(e){
     e.preventDefault();
+
+    var firstName = $("#first-name").val();
+    var lastName = $("#last-name").val();
+    var address = $("#address").val();
+    var item = $("input:radio[name=dinosaur]:checked").val();
+    item = firstLetterCaps(item);
+    addOrderDetails(firstName, lastName, address, item);
   });
 });
